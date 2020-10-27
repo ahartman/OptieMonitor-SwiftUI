d19b68ac80ff248b1a4bbd1b0e09506e0090be84
André Hartman <ahartman@skynet.be>
2020-10-27 08:40:23 +0100

    Initial Commit

---
+++ OptieMonitor/OptieMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptieMonitorApp.swift
+//  OptieMonitor
+//
+//  Created by André Hartman on 27/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct OptieMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
